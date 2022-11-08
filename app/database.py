a5a3a6692df731af4ff345a9a3628da32e48ccc5
luckoff <neluckoff@gmail.com>
2022-11-08 17:15:02 +0300

    updated load to machine

--- app/database.py
+++ app/database.py
@@ -3,21 +3,28 @@ import time
 
 class Database:
     def __init__(self) -> None:
-        try:
-            self.connection = pymysql.connect(
-                host='127.0.0.1',
-                port=3306,
-                user='root',
-                password='Dmitry9260304',
-                database='vending_workbench'
-            )
-        except Exception as ex:
-            print(ex)
+        self.connection = pymysql.connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='Dmitry9260304',
+            database='vending_workbench'
+        )
             
-        self.machines = self.__generate_machines()
+        self.machines_normal = self.__generate_machines()
+        self.machines_problem = self.__generate_machines_with_problems()
         self.problems = self.__generate_problems()
             
     def __generate_machines(self) -> list:
+        machines = []
+        with self.connection.cursor() as cursor:
+            cursor.execute("SELECT `id`, `geo` FROM `automat` WHERE `condition` = 0")
+            rows = cursor.fetchall()
+            for row in rows:
+                machines.append([row[0], row[1]])
+        return machines  
+    
+    def __generate_machines_with_problems(self) -> list:
         machines = []
         with self.connection.cursor() as cursor:
             cursor.execute("SELECT `id`, `geo` FROM `automat`")
@@ -36,7 +43,10 @@ class Database:
         return problems
     
     def get_machines(self) -> list:
-        return self.machines
+        return self.machines_normal
+    
+    def get_machines_prob(self) -> list:
+        return self.machines_problem
     
     def get_problems(self) -> list:
         return self.problems
@@ -67,3 +77,38 @@ class Database:
                 products.append([row[0], f'{row[1]} ({row[2]})'])
                 
             return products
+        
+    def get_all_products(self) -> list:
+        with self.connection.cursor() as cursor:
+            products = []
+            cursor.execute(f'SELECT product.id, product.name, product.description FROM product')
+            
+            for row in cursor.fetchall():
+                products.append([row[0], f'{row[1]} ({row[2]})'])
+                
+            return products
+        
+    def new_machine(self, geo: str, geo_desc: str, install_date: str, max_products: str) -> None:
+        with self.connection.cursor() as cursor:
+            cursor.execute(f'INSERT INTO `automat` (`geo`, `geo_description`, `installation_date`, `condition`, `max_number_products`)'
+                           f'VALUES ("{geo}", "{geo_desc}", "{install_date}", {0}, "{max_products}")')
+            self.connection.commit()
+            
+    def new_product(self, name: str, price: float, desc: str, date_added: str, before_date: str) -> None:
+        with self.connection.cursor() as cursor:
+            cursor.execute(f'INSERT INTO `product` (`name`, `price`, `description`, `date_added`, `best_before_date`) '
+                           f'VALUES ("{name}", {price}, "{desc}", "{date_added}", "{before_date}")')
+            self.connection.commit()
+            
+    def add_product_to_machine(self, product_id: int, automat_id: int, amount: int) -> None:
+        with self.connection.cursor() as cursor:
+            cursor.execute(f'SELECT `automat_id`, `product_id` FROM `automat_products` WHERE `automat_id` = {automat_id} AND `product_id` = {product_id}')
+            result = cursor.fetchall()
+            if len(result) > 0:
+                cursor.execute(f'SELECT `amount` FROM `automat_products` WHERE `automat_id` = {automat_id} AND `product_id` = {product_id}')
+                old_amount = cursor.fetchall()[0][0]
+                res_amount = old_amount + amount
+                cursor.execute(f'UPDATE `automat_products` SET `amount` = {res_amount} WHERE `automat_id` = {automat_id} AND `product_id` = {product_id}')
+            else:
+                cursor.execute(f'INSERT INTO `automat_products` (`product_id`, `automat_id`, `amount`) VALUES ({product_id}, {automat_id}, {amount})')
+            self.connection.commit()
--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ db = Database()
 @app.route('/', methods=['GET'])
 def index():
     machines = db.get_machines()
+    machines_pr = db.get_machines_prob()
     problems = db.get_problems()
     products = db.get_products(1)
-    return render_template('index.html', machines=machines, problems=problems, products=products)
+    return render_template('index.html', machines=machines, problems=problems, products=products, machines_pr=machines_pr)
     
 
 @app.route('/problem', methods=['POST'])
@@ -18,8 +19,8 @@ def problem():
     machine = request.form['select-machine']
     problem = request.form['select-problem']
     text = request.form['problem-desc']
-    # db.set_break(automat_id=machine, break_id=problem, desc_break=text)
-    return "Жалоба отправлена!"
+    db.set_break(automat_id=machine, break_id=problem, desc_break=text)
+    return render_template('confirm-page.html', head="Вы успешно отправили жалобу на автомат!", text=f"Жалоба на автомат {machine} была успешно направлена на рассмотрение!", btn_text="Вернуться на главную", href="/")
     
     
 @app.route('/order', methods=['POST'])
@@ -27,22 +28,45 @@ def order():
     machine = request.form['select-machine-to-buy']
     product = request.form['select-product']
     print(machine, problem)
-    return render_template('confirm-page.html', head="Вы успешно приобрели товар!", text=f"Товар {product} был успешно приобретен в автомате №{machine}!", btn_text="Вернуться на главную")
+    return render_template('confirm-page.html', head="Вы успешно приобрели товар!", text=f"Товар {product} был успешно приобретен в автомате №{machine}!", btn_text="Вернуться на главную", href="/")
 
 @app.route('/admin', methods=['GET'])
 def admin():
-    machines = db.get_machines()
-    problems = db.get_problems()
-    products = db.get_products(1)
-    return render_template('admin.html', machines=machines, problems=problems, products=products)
+    machines = db.get_machines_prob()
+    all_products = db.get_all_products()
+    return render_template('admin.html', machines=machines, products=all_products)
         
 @app.route('/admin/machine', methods=['POST'])
 def admin_machine():
-    return "Автомат добавлен!"
+    geo = request.form['geo-machine']
+    geo_desc = request.form['geo-decs-machine']
+    ins_date = request.form['install-date']
+    max_prod = request.form['max-products']
+    db.new_machine(geo, geo_desc, ins_date, max_prod)
+    return render_template('confirm-page.html', head=f"Автомат был успешно добавлен!", text=f"Вы успешно добавили автомат по адресу {geo},"
+                           f" дата установки: {ins_date}, максимально продуктов: {max_prod}", btn_text="В Админ-Панель", href="/admin")
 
 @app.route('/admin/product', methods=['POST'])
 def admin_product():
-    return "Продукт добавлен!"
+    prod = request.form['prod']
+    price = request.form['price-prod']
+    desc = request.form['desc']
+    added = request.form['date-added']
+    before = request.form['before']
+    db.new_product(prod, price, desc, added, before)
+    return render_template('confirm-page.html', head=f"Продукт был успешно добавлен!", text=f"Вы успешно добавили продукт {prod},"
+                           f" цена: {price}, описание: {desc}, дата добавления: {added}, срок годности: {before}", btn_text="В Админ-Панель", href="/admin")
+    
+@app.route('/admin/load', methods=['POST'])
+def admin_load():
+    machine = request.form['select-machine']
+    product = request.form['select-product']
+    num = request.form['prod-num']
+    print(f'{machine} - {product} - {num}')
+    db.add_product_to_machine(automat_id=int(machine), product_id=int(product), amount=int(num))
+    return render_template('confirm-page.html', head=f"Продукт был успешно загружен!", text=f"Вы успешно добавили продукт №{product},"
+                           f" в автомат №{machine} в количестве {num} штук.", btn_text="В Админ-Панель", href="/admin")
+    
 
 if __name__ == '__main__':
     app.run()
